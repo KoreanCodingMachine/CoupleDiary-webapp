4ec0a070b9a1a8c358ec570af116a20975350e7e
KoreanCodingMachine <59787529+KoreanCodingMachine@users.noreply.github.com>
2022-03-21 15:59:06 +0900

    addsignuploginlogout

--- couplediary-react/src/component/CardComponent.js
+++ couplediary-react/src/component/CardComponent.js
@@ -7,12 +7,13 @@ export default class CardComponent extends React.Component {
   render() {
     return (
       <>
-        <Card className='card' style={{ margin: '8px', width: '18rem' }}>
+        <Card className='card' style={{ margin: '8px', width: '29rem' }}>
           <Card.Img variant='top' src='../logo192.png' />
           {/* src = this.props.card.image */}
           <Card.Body>
             <Card.Title>{this.props.card.title}</Card.Title>
             <Card.Text>{this.props.card.text}</Card.Text>
+
             <Link to='CardUpdate'>
               <Button variant='primary'>카드 수정 하기</Button>
             </Link>
